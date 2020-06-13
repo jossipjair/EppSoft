f5b66f3193756f24657d8eb90f40fa3194bbfacc
jescalaya <jescalaya@adr.com.pe>
2020-06-12 20:22:29 -0500

    Commit test

---
+++ EppSoft/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EppSoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
